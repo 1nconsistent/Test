557c785eee144e4102660478d5313d999e31e1a5
Akhil P V <akhilpv2334768@gmail.com>
2026-01-08 17:10:07 +0530

    custom header files instead of individual function declarations

--- Intro/Log.cpp
+++ Intro/Log.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include "Log.h"
+
+void InitLog()
+{
+	Log("Log initialized");
+}
 void Log(const char* message)
 {
 	std::cout << message << std::endl;
--- Intro/Main.cpp
+++ Intro/Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "Log.h"
+
 void Log(const char*);
 int Multiply(int, int);
+
 int main()
 {
 	int a, b;
