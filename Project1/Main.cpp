89adf6310718f907bf6007bb6d3c5047c37ad69d
Akhil P V <akhilpv2334768@gmail.com>
2026-01-08 12:32:57 +0530

    expermenting with user inputs + linkers

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -3,9 +3,10 @@ void Log(const char*);
 int Add(int, int);
 int main()
 {
-	int a = (Add(3, 4));
-	std::cout << a << std::endl;
+	int a, b;
+	std::cin >> a >> b;
+	int c = (Add(a, b));
+	std::cout << c << std::endl;
 	Log("Hello, testing 123");
 	getchar();
 }
-
