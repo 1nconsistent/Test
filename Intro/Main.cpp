c12cff0d0b0c4ec9332cebd35ff40fe5843b866a
aagneyanNamboodiri <aagu.cotton@gmail.com>
2026-01-10 16:55:54 +0530

    Familiarisation with breakpoints and debugging

--- Intro/Main.cpp
+++ Intro/Main.cpp
@@ -15,12 +15,12 @@ int main()
 	float b = 3.5;
 	InitLog();
 	std::cin >> a;
-	float c = (Multiply(a, b));
+	float c = (Multiply(a, b)); //Breakpoint1
 	std::cout << c << std::endl;
 	a = 12.5;
 	c = Multiply(a, b);
 	std::cout << c << std::endl;
-	Log("Divide now you idiot");
+	Log("Divide now you idiot");	//Breakpoint2
 	int d1, d2;
 	std::cin >> d1 >> d2;
 	int Div = Divide(d1, d2);
