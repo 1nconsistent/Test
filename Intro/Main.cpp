654cb940b59860a55b8ca76521815005205d73d9
Akhil P V <akhilpv2334768@gmail.com>
2026-01-12 16:39:54 +0530

    Working with conditional statements / user inputs and conditions

--- Intro/Main.cpp
+++ Intro/Main.cpp
@@ -11,31 +11,39 @@ int Divide(int da, int db)
 
 int main()
 {
-	float a;
-	float b = 3.5;
+	int condi;
+	float a, b;
 	InitLog();
-	std::cin >> a;
-	float c = (Multiply(a, b)); //Breakpoint1
-	std::cout << c << std::endl;
-	a = 12.5;
-	c = Multiply(a, b);
-	std::cout << c << std::endl;
-	Log("Divide now you idiot");	//Breakpoint2
-	int d1, d2;
-	std::cin >> d1 >> d2;
-	int Div = Divide(d1, d2);
-	std::cout << "Div: " << Div << std::endl;
-	Log("Hello, testing 123");
-	
-	int ad[] = { 5,3,2,4,3,6 };
-	std::cout << "Array of Numbers: " << std::endl;
-	for (int i = 0; i < 6; i++)
+	std::cout << "\nDo you want to multiply(1) or divide(2)?" << std::endl;
+	std::cin >> condi;
+	if (condi == 1)
 	{
-		std::cout << ad[i] << std::endl;
+		Log("Give 2 numbers to multiply");
+		std::cin >> a >> b;
+		float c = (Multiply(a, b)); //Breakpoint1
+		std::cout << "Value: " << c << std::endl;
 	}
-	const char* aag = "Quadratini";
-	for (int i = 0; i < 10; i++)
+	else if (condi == 2)
 	{
+		Log("Give 2 numbers to divide");	//Breakpoint2
+		int d1, d2;
+		std::cin >> d1 >> d2;
+		int Div = Divide(d1, d2);
+		std::cout << "Div: " << Div << std::endl;
+	}
+	else
+	{
+		Log("Invalid option \n Here's a random output:");
+		int ad[] = { 5,3,2,4,3,6 };
+		std::cout << "Array of Numbers: " << std::endl;
+		for (int i = 0; i < 6; i++)
+		{
+			std::cout << ad[i] << std::endl;
+		}
+		const char* aag = "Quadratini";
+		for (int i = 0; i < 10; i++)
+		{
 			std::cout << aag[i] << std::endl;
+		}
 	}
 }
