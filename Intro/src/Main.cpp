c1546014e0861d75c1d554798f16b59ea72d472e
Akhil P V <akhilpv2334768@gmail.com>
2026-01-17 20:39:27 +0530

    Pointer issue fixed

--- Intro/src/Main.cpp
+++ Intro/src/Main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 #include "Log.h"
 #include "MathCustom.h"
-//whatever, random commit
+
 int main()
 {
+	char c = 'Y';
+	do
+	{
+		Log("Working with pointers");
+		int* x = new int(10);
+		int* y = new int(20);
+		Log("Enter two numbers: ");
+		std::cin >> *x;
+		std::cin >> *y;
+		std::cout << "Memory Address of x: " << x << " Value at x: " << *x << std::endl;
+		std::cout << "Memory Address of y: " << y << " Value at y: " << *y << std::endl;
+		char s = 'a';
+		for (; s != 'N' && s != 'n'; )
+		{
+			int a = *x + *y;
+			std::cout << "Sum of values: " << a << std::endl;
+			*x = a;
+			*y = *x;
+			std::cout << "New value at x: " << *x << std::endl;
+			std::cout << "New value at y (address of x): " << *y << std::endl;
+			Log("Do you want to continue adding? (Y/N): ");
+			std::cin >> s;
+		}
+		delete x;
+		delete y;
+		std::cout << "Do you want to repeat? (Y/N): " << std::endl;
+		std::cin >> c;
+	} while (c == 'Y' || c == 'y');
+
 	int condi;
 	float a, b;
 	do
